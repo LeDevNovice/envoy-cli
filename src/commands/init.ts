c7676da8f0bd1458610f7a18b0bbb77f5139fcf2
LeDevNovice <gregory.saison@proton.me>
2025-10-04 10:55:39 +0200

    feat: add logger in init command

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,7 +1,18 @@
 import { Command } from "commander";
+import { Logger } from "../utils/logger";
 
 export function createInitCommand(): Command {
     return new Command('init')
         .description('Initialize the envoy configuration in the current project')
-        .action(async () => { });
+        .action(async () => {
+            Logger.header('Initializing envoy-cli configuration...')
+
+            Logger.success('envoy-cli has been successfully initialized in your project !');
+            Logger.info('Next steps:');
+            Logger.list([
+                'First step',
+                'Second step',
+                'Third step'
+            ])
+        });
 }
